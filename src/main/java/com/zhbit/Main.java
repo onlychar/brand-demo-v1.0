b30137f26c762c657d1613244b80627f28c023cf
charlie <java_assign@qq.com>
2023-02-13 10:37:41 +0800

    push test 3

--- src/main/java/com/zhbit/Main.java
+++ src/main/java/com/zhbit/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("push test !");
         System.out.println("pull test !");
         System.out.println("pull test2  !");
+        System.out.println("pull test3  !");
     }
 }
