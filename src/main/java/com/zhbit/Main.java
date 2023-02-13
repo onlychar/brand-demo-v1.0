36a63b5f7c2b86d93539bf1202a3efe3edd19415
charlie <java_assign@qq.com>
2023-02-13 10:15:21 +0800

    first commit

--- src/main/java/com/zhbit/Main.java
+++ src/main/java/com/zhbit/Main.java
@@ -2,6 +2,9 @@ package com.zhbit;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("----------------------------------" );
+        System.out.println("push test !");
     }
 }
